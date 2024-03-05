31c8691d3f5b596ced99b856123a82a4117c4eaa
Ash Joseph <kamikazedesigns@gmail.com>
2024-03-05 22:53:32 +0000

    Some basic relationships

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
+class Tag extends Model
 {
     use HasFactory;
 
-    public function tasks()
-    {
-        return $this->hasMany(Task::class);
+    public function tasks(){
+        return $this->belongsToMany(Task::class);
     }
 }
