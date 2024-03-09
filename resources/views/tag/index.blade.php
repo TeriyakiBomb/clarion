71139a8d42d38d3cdf18b219a5277e5972954439
Ash Joseph <kamikazedesigns@gmail.com>
2024-03-09 10:27:17 +0000

    Add link to tag index

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @foreach($tags as $tag)
-                        {{$tag->name}}
+                        <a class="cursor-pointer" href="/tag/{{$tag->id}}" wire:navigate>{{$tag->name}}</a>
                     @endforeach
                 </div>
             </div>
