5a26bd03a25ed659e26b6bd38197468495b69a70
Ash Joseph <kamikazedesigns@gmail.com>
2024-03-09 10:33:18 +0000

    Improved tag.show view

--- resources/views/tag/show.blade.php
+++ resources/views/tag/show.blade.php
@@ -1,6 +1,22 @@
-<h1>{{$tag->name}}</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            TAG - {{$tag->name}}
+        </h2>
+    </x-slot>
 
-@foreach($tag->tasks as $task)
-    {{$task->name}}
-@endforeach
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h1>{{$tag->name}}</h1>
+
+                    @foreach($tag->tasks as $task)
+                        {{$task->name}}
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 
